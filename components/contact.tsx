70404f9cbf3d32c32711b0579cf0a209876e2bf7
NAREN SUNDAR L <66767561+NarenSundar007@users.noreply.github.com>
2025-05-27 18:00:03 +0530

    value changes in about

--- components/contact.tsx
+++ components/contact.tsx
@@ -68,7 +68,7 @@ export default function Contact() {
               </div>
               <div className="flex items-center gap-3">
                 <Mail className="h-5 w-5 text-primary" />
-                <Link href="mailto:info@inteliotclub.edu" className="hover:underline">
+                <Link href="mailto:inteliotclub@gmail.com" className="hover:underline">
                   inteliotclub@gmail.com
                 </Link>
               </div>
